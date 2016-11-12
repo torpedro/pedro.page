5e0caccc4c654757116b3ddfc33268df8c6837f2
Pedro Flemming <pedroflemming@gmail.com>
2016-11-12 15:09:27 +0100

    Format indentation to 2 spaces.

--- src/CSSTransform.ts
+++ src/CSSTransform.ts
@@ -1,29 +1,29 @@
 
 export class CSSTransform {
-	private el: HTMLElement;
-	private perspective: number;
-	private transforms: string[] = [];
+  private el: HTMLElement;
+  private perspective: number;
+  private transforms: string[] = [];
 
-	constructor(element: HTMLElement, perspective?: number) {
-		this.el = element;
-		this.perspective = perspective;
-		this.clear();
-	}
+  constructor(element: HTMLElement, perspective?: number) {
+    this.el = element;
+    this.perspective = perspective;
+    this.clear();
+  }
 
-	clear(): CSSTransform {
-		this.transforms = [];
-        return this;
-	}
+  clear(): CSSTransform {
+    this.transforms = [];
+    return this;
+  }
 
-	apply(): CSSTransform {
-		this.el.style.transform = `perspective(${this.perspective}px) ${this.transforms.join(" ")}`;
-        return this;
-	}
+  apply(): CSSTransform {
+    this.el.style.transform = `perspective(${this.perspective}px) ${this.transforms.join(" ")}`;
+    return this;
+  }
 
-	rotate(degreesX: number, degreesY: number, degreesZ: number): CSSTransform {
-		if (degreesX) this.transforms.push(`rotateX(${degreesX}deg)`);
-		if (degreesY) this.transforms.push(`rotateY(${degreesY}deg)`);
-		if (degreesZ) this.transforms.push(`rotateZ(${degreesZ}deg)`);
-        return this;
-	}
+  rotate(degreesX: number, degreesY: number, degreesZ: number): CSSTransform {
+    if (degreesX) this.transforms.push(`rotateX(${degreesX}deg)`);
+    if (degreesY) this.transforms.push(`rotateY(${degreesY}deg)`);
+    if (degreesZ) this.transforms.push(`rotateZ(${degreesZ}deg)`);
+    return this;
+  }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -3,26 +3,26 @@ var webpack = require('webpack');
 var PROD = (process.env.NODE_ENV === 'production')
 
 module.exports = {
-    entry: "./src/app.ts",
-    output: {
-        path: __dirname,
-        filename: "bundle.js"
-    },
-    resolve: { 
-        // lists the extensions of files to look for in requires
-        extensions: ['', '.webpack.js', '.web.js', '.ts', '.tsx', '.js']
-    },
-    module: {
-        loaders: [
-            { test: /\.ts$/, loader: 'ts-loader' },
-            { test: /\.tsx$/, loader: 'ts-loader' },
-            { test: /\.css$/, loader: "style!css" },
-            { test: /\.scss$/, loaders: ["style", "css?-url", "sass"] }, // ?-url disables url(...) handling
-        ]
-    },
-    plugins: PROD ? [
-        new webpack.optimize.UglifyJsPlugin({
-            compress: { warnings: false }
-        })
-    ] : []
+  entry: "./src/app.ts",
+  output: {
+    path: __dirname,
+    filename: "bundle.js"
+  },
+  resolve: { 
+    // lists the extensions of files to look for in requires
+    extensions: ['', '.webpack.js', '.web.js', '.ts', '.tsx', '.js']
+  },
+  module: {
+    loaders: [
+      { test: /\.ts$/, loader: 'ts-loader' },
+      { test: /\.tsx$/, loader: 'ts-loader' },
+      { test: /\.css$/, loader: "style!css" },
+      { test: /\.scss$/, loaders: ["style", "css?-url", "sass"] }, // ?-url disables url(...) handling
+    ]
+  },
+  plugins: PROD ? [
+    new webpack.optimize.UglifyJsPlugin({
+      compress: { warnings: false }
+    })
+  ] : []
 };
