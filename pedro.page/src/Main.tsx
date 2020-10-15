bc152df598f6007710f5b48fa042d2c9647339fd
Pedro <pedroflemming@gmail.com>
2020-10-15 03:18:49 +0100

    add drawer

--- pedro.page/src/Main.tsx
+++ pedro.page/src/Main.tsx
@@ -14,31 +14,48 @@ export class Main extends React.Component<IMainProps, {}> {
         return (
             <div id="main">
                 <div id="brand">
-                    <div id="name">
-                        <h1>Pedro Flemming</h1>
-                        <h2>Software Engineer in London, UK</h2>
-                    </div>
-                    <div id="icons">
-                        <a target="blank" className="icon linkedin" href="https://www.linkedin.com/in/pedroflemming"></a>
-                        <a target="blank" className="icon github" href="https://github.com/torpedro"></a>
-                        <a target="blank" className="icon instagram" href="https://www.instagram.com/thetorpedro/"></a>
+                    <div>
+                        <div id="name">
+                            <h1>Pedro Flemming</h1>
+                            <h2>Software Engineer in London, UK</h2>
+                        </div>
+                        <div id="icons">
+                            <a target="blank" className="icon linkedin" href="https://www.linkedin.com/in/pedroflemming"></a>
+                            <a target="blank" className="icon github" href="https://github.com/torpedro"></a>
+                            <a target="blank" className="icon instagram" href="https://www.instagram.com/thetorpedro/"></a>
+                        </div>
                     </div>
                 </div>
-                {/* <div id="projects">
-                    <p>Currently I work at Jane Street in London, UK. Here's a selection of my past projects.</p>
-                    <a target="blank" href="https://github.com/hyrise/sql-parser">
-                        <h1>SQL Parser</h1>
-                        <h2>C++ Library</h2>
-                    </a>
-                    <a target="blank" href="https://github.com/torpedro/gdrive-lib">
-                        <h1>gdrive</h1>
-                        <h2>CLI &amp; Python Library</h2>
-                    </a>
-                    <a target="blank" href="https://github.com/torpedro/microtest.h">
-                        <h1>microtest.h</h1>
-                        <h2>C++ Library</h2>
-                    </a>
-                </div> */}
+                <div id="more">
+                    <div id="drawer">
+                        <div id="left"></div>
+                        <div id="right"></div>
+                    </div>
+                    <div id="content">
+                        <div id="projects">
+                            <a target="blank" href="https://github.com/hyrise/sql-parser">
+                                <h1>SQL Parser</h1>
+                                <h2>C++ Library</h2>
+                            </a>
+                            <a target="blank" href="https://github.com/torpedro/gdrive-lib">
+                                <h1>gdrive</h1>
+                                <h2>CLI &amp; Python Library</h2>
+                            </a>
+                            <a target="blank" href="https://hpi.de/plattner/projects/project-archive/pos-explorer.html">
+                                <h1>POS Explorer</h1>
+                                <h2>ERP Application</h2>
+                            </a>
+                            <a target="blank" href="https://github.com/torpedro/microtest.h">
+                                <h1>microtest.h</h1>
+                                <h2>C++ Library</h2>
+                            </a>
+                            <a target="blank" href="https://epic-circles.torpedro.com">
+                                <h1>Epic Circles</h1>
+                                <h2>JavaScript</h2>
+                            </a>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
