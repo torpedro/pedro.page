e72a4b0d18c2194739949d1ee0b910cc4ff11cbf
Pedro <pedroflemming@gmail.com>
2021-10-03 20:49:16 +0100

    update link

--- pedro.page/src/Main.tsx
+++ pedro.page/src/Main.tsx
@@ -29,7 +29,7 @@ export class Main extends React.Component<IProps, IState> {
                                     <div id="icons">
                                         <a className="icon linkedin" href="https://www.linkedin.com/in/pedroflemming"></a>
                                         <a className="icon github" href="https://github.com/torpedro"></a>
-                                        <a className="icon instagram" href="https://www.instagram.com/thetorpedro/"></a>
+                                        <a className="icon instagram" href="https://www.instagram.com/pedros.pictures/"></a>
                                     </div>
                                 </div>
                             </div>
